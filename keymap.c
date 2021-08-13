f2c0096a35557f2aacf27f101a784aec6ca0f9c0
J-Heise <johannes.erik.heise@gmail.com>
2021-08-13 15:32:07 +0200

    Fixed typos and formatting

--- keymap.c
+++ keymap.c
@@ -1,13 +1,13 @@
 //Written for Keyz.io SKErgo rev. 1
 
-//Hold boot and+ press reset to enter bootloader mode//
+//Hold boot and press reset to enter bootloader mode.
 //MCU: ATMEGA32A
 
 #include QMK_KEYBOARD_H
 
 enum custom_keycodes
 {
-	//SAFE_RANGE macro guarantees custom keycodes to be unique
+	//SAFE_RANGE macro guarantees custom keycodes to be unique.
 	DIACRITIC_A = SAFE_RANGE, 
 	DIACRITIC_U,
 	DIACRITIC_O,
@@ -19,7 +19,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record)
 {
   switch (keycode)
   {
-	//Macro for ä and Ä
+	//Macro for ä and Ä.
 	case DIACRITIC_A:
     if(record->event.pressed)
 		{
@@ -35,7 +35,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record)
 		}
     return false;
 
-	//Macro for ü and Ü
+	//Macro for ü and Ü.
     case DIACRITIC_U:
     if(record->event.pressed)
 		{
@@ -51,7 +51,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record)
 		}
     return false;
 
-	//Macro for ö and Ö
+	//Macro for ö and Ö.
     case DIACRITIC_O:
     if(record->event.pressed)
 		{
@@ -67,7 +67,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record)
 		}
     return false;
 
-	//Macro for ß 
+	//Macro for ß.
     case DOUBLE_S:
     if(record->event.pressed)
 		{
@@ -83,7 +83,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record)
 		}
     return false;
 
-	//Macro for €
+	//Macro for €.
     case EURO_CURRENCY_SIGN:
     if(record->event.pressed)
 		{
@@ -106,7 +106,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = 
 {
-	//Default Layer
+	//Default Layer:
 	[0] =	LAYOUT(
 			KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_GRV, KC_BSPC,
 
@@ -118,8 +118,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
             
 			KC_LCTL, KC_LGUI, KC_SPC, KC_LALT, KC_SPC, MO(1), KC_LEFT, KC_DOWN, KC_RGHT),
 
-	//F1 Layer
-	//Accessed through pressing MO(1)
+	//F1 Layer:
+	//Accessed through pressing MO(1).
 	[1] =	LAYOUT(
 			KC_SYSTEM_WAKE, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_TRNS, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_TRNS, 
 
